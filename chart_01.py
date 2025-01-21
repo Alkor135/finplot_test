f8f7c96a2c494b1ad647c18ec35cb03ba9ced231
Aleksey Manin <Alkor135@gmail.com>
2025-01-22 00:42:07 +0300

    Add Volume Stops

--- chart_01.py
+++ chart_01.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import finplot as fplt
+from datetime import timezone
+
+fplt.display_timezone = timezone.utc  # Настройка тайм зоны, чтобы не было смещения времени
 
 
 def adaptive_laguerre_filter(df, alpha=0.3):
@@ -45,29 +48,72 @@ def adaptive_laguerre_filter(df, alpha=0.3):
     return df
 
 
-alpha=0.4
+def volume_stops(df):
+    # Инициализация новых колонок значением None
+    df['long1'] = None
+    df['short1'] = None
+    df['long2'] = None
+    df['short2'] = None
 
-df = pd.read_csv('cache.csv')
-df['datetime'] = pd.to_datetime(df['datetime'])
-# df = df.set_index('datetime')
-df = df.rename(columns={'vol':'volume'})
-df = adaptive_laguerre_filter(df, alpha=alpha)
-print(df)
+    # Проверка условий для каждой строки
+    for i in range(2, len(df)):
+        if (df['volume'][i - 2] < df['volume'][i - 1] < df['volume'][i] and
+                df['open'][i - 2] >= df['close'][i - 2] and df['open'][i - 1] >= df['close'][i - 1] and
+                df['open'][i] <= df['close'][i]):
+            df.at[i, 'long1'] = df['low'][i] - 30
 
-# create two axes
-ax = fplt.create_plot('RTS', rows=1)
+        if (df['volume'][i - 2] < df['volume'][i - 1] < df['volume'][i] and
+                df['open'][i - 2] <= df['close'][i - 2] and df['open'][i - 1] <= df['close'][i - 1] and
+                df['open'][i] >= df['close'][i]):
+            df.at[i, 'short1'] = df['high'][i] + 30
 
-# plot candle sticks
-# candles = df[['open', 'close', 'high', 'low']]
-candles = df[['datetime', 'open', 'close', 'high', 'low']]
-fplt.candlestick_ochl(candles, ax=ax)
+        if (df['volume'][i - 2] > df['volume'][i - 1] > df['volume'][i] and
+                df['open'][i - 2] >= df['close'][i - 2] and df['open'][i - 1] >= df['close'][i - 1] and
+                df['open'][i] <= df['close'][i]):
+            df.at[i, 'long2'] = df['low'][i] - 30
 
-# overlay volume on the top plot
-# volumes = df[['open','close','volume']]
-volumes = df[['datetime', 'open','close','volume']]
-fplt.volume_ocv(volumes, ax=ax.overlay())
+        if (df['volume'][i - 2] > df['volume'][i - 1] > df['volume'][i] and
+                df['open'][i - 2] <= df['close'][i - 2] and df['open'][i - 1] <= df['close'][i - 1] and
+                df['open'][i] >= df['close'][i]):
+            df.at[i, 'short2'] = df['high'][i] + 30
 
-# put an ALF on the close price
-fplt.plot(df['datetime'], df['alf'], ax=ax, legend=f'ALF-{alpha}')
+    return df
 
-fplt.show()
+
+if __name__ == '__main__':
+    alpha=0.4
+
+    df = pd.read_csv('cache.csv')
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    # df = df.set_index('datetime')
+    df = df.rename(columns={'vol':'volume'})
+    df = adaptive_laguerre_filter(df, alpha=alpha)
+    df = volume_stops(df)
+    # Сохранение DF в файл без индекса
+    df.to_csv(fr'alf_vs.csv', index=False)
+    print(df)
+
+    # create two axes
+    ax = fplt.create_plot('RTS', rows=1)
+    ax.set_visible(xgrid=True, ygrid=True)
+
+    # plot candle sticks
+    # candles = df[['open', 'close', 'high', 'low']]
+    candles = df[['datetime', 'open', 'close', 'high', 'low']]
+    fplt.candlestick_ochl(candles, ax=ax)
+
+    # overlay volume on the top plot
+    # volumes = df[['open','close','volume']]
+    volumes = df[['datetime', 'open','close','volume']]
+    fplt.volume_ocv(volumes, ax=ax.overlay())
+
+    # put an ALF on the close price
+    fplt.plot(df['datetime'], df['alf'], ax=ax, legend=f'ALF-{alpha}')
+
+    # Volume Stops
+    fplt.plot(df['datetime'], df['long1'], legend='Long 1 Max volume', style='o', color='#00f')
+    fplt.plot(df['datetime'], df['long2'], legend='Long 2 Min Volume', style='^', color='#00f')
+    fplt.plot(df['datetime'], df['short1'], legend='Short 1 Max volume', style='o', color='#00f')
+    fplt.plot(df['datetime'], df['short2'], legend='Short 2 Min Volume', style='^', color='#00f')
+
+    fplt.show()
